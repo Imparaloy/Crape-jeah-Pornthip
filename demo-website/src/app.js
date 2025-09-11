7e06c8231dbd07c107da0fc7bdee15d938ad1cbf
Sirikul Khummung <66070192@kmitl.ac.th>
2025-09-11 21:42:25 +0700

    simple ngub

---
+++ demo-website/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// ตั้งค่า static ให้ public
+app.use(express.static(path.join(__dirname, '../public')));
+
+// ตั้งค่า view engine เป็น ejs
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+// ตัวอย่าง route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+module.exports = app;
--- demo-website/tailwind.config.js
+++ demo-website/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 content: [
-"./public/**/*.{html,js}",
-"./src/**/*.{js,ts}"
+	"./public/**/*.{html,js}",
+	"./src/**/*.{js,ts}",
+	"./views/**/*.ejs"
 ],
 theme: {
 extend: {},
